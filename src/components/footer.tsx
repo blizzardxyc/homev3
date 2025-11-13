17dcbf9ec26734d141e92ed4fc80549954a3f339
blizzardxyc <deepabhyudaya@gmail.com>
2025-11-13 22:43:59 +0530

    *

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import { SmoothCursorSwitch } from './SmoothCursorSwitch';
+
 function Footer() {
     return (
-        <div className='flex h-auto min-h-12 w-full items-center justify-center pt-24 pb-4'>
+        <div className='flex h-auto min-h-12 w-full flex-col items-center justify-center gap-6 space-x-4 pt-24 pb-4'>
+            <span className='hidden md:block lg:block'>
+                <SmoothCursorSwitch />
+            </span>
             <h1 className='text-sm font-medium'>©Abhyudaya 2022-2025</h1>
         </div>
     );
